84a0e2187675379a16c1e1b67409ee6c99e2c1ba
Luke Wickens <lukewickens1989@gmail.com>
2020-11-10 13:20:08 +0000

    completed fizzbuzz with testing

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,16 @@
 def fizzbuzz(number)
-  "fizz"
+      if number % 3 == 0 && number % 5 == 0
+      return "fizzbuzz"
+    elsif number % 3 == 0
+      return "fizz"
+    elsif number % 5 == 0
+      return "buzz"
+    else
+      return number
+  end
 end
+
+fizzbuzz(3)
+fizzbuzz(5)
+fizzbuzz(15)
+fizzbuzz(4)
